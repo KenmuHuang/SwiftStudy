ee07ff86607e2ab38394b2e02af0b707d0bba0da
huangjianwu <kenmuhuang@xinli001.com>
2020-06-01 21:36:55 +0800

    [test]变量和常量；https://docs.swift.org/swift-book/GuidedTour/GuidedTour.html

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         updateImage()
+        testVariableAndConstantType()
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
@@ -38,6 +39,45 @@ class ViewController: UIViewController {
         if randomImageNameIndex < arrImageName.count {
             imgVAvatar.image = UIImage(named: arrImageName[randomImageNameIndex])
         }
+    }
+
+    func testVariableAndConstantType() -> Void {
+        print("Hello, world!")
+
+        var myVariable = 42
+        myVariable = 50
+
+        let myConstant = 42
+
+        let implicitInteger = 70
+        let implicitDouble = 70.0
+
+        let label = "The width is "
+        let width = 94
+        let widthLabel = label + String(width)
+
+        let apples = 3
+        let oranges = 5
+        let appleSummary = "I have \(apples) apples."
+        let fruitSummary = "I have \(apples + oranges) pieces of fruits."
+        let quotation = """
+                        I said "I have \(apples) apples."
+                        And then I said "I have \(apples + oranges) pieces of fruits."
+                        """
+
+        var shoppingList = ["catfish", "water", "tulips"]
+        shoppingList[1] = "bottle of water"
+        shoppingList.append("blue paint")
+        print(shoppingList)
+
+        var occupations = [
+            "Malcolm": "Captain",
+            "Kaylee": "Mechanic",
+        ]
+        occupations["Jayne"] = "Public Relations"
+
+        let emptyArray = [String]()
+        let emptyDictionary = [String: Float]()
     }
     
     @IBAction func onRandomImageButtonClicked(_ sender: Any) {
