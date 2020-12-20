549007e845036d8d910824408082d33ab3184e4f
huangjianwu <kenmuhuang@xinli001.com>
2020-12-20 21:29:17 +0800

    [test] 闭包 - 闭包表达式：简化写法；https://swiftgg.gitbook.io/swift/swift-jiao-cheng/07_closures

--- SwiftStudy/Language/Detail/Closures.swift
+++ SwiftStudy/Language/Detail/Closures.swift
@@ -28,13 +28,28 @@ class Closures: MainProtocol {
     
     func sort(names: [String], isDescending: Bool) -> [String] {
         if isDescending {
-            return names.sorted(by: {(s1: String, s2: String) -> Bool in
-                return s1 > s2
-            })
+//            return names.sorted(by: {(s1: String, s2: String) -> Bool in
+//                return s1 > s2
+//            })
+
+            // 运算符方法
+            return names.sorted(by: >)
         } else {
-            return names.sorted(by: {(s1: String, s2: String) -> Bool in
-                return s1 < s2
-            })
+//            return names.sorted(by: {(s1: String, s2: String) -> Bool in
+//                return s1 < s2
+//            })
+
+            // 根据上下文推断类型，单表达式闭包的隐式返回
+//            return names.sorted { s1, s2 in s1 < s2 }
+
+            // 参数名称缩写
+//            return names.sorted(by: { $0 < $1 })
+
+            // 运算符方法
+//            return names.sorted(by: <)
+
+            // 使用默认升序函数
+            return names.sorted()
         }
 
 //        return names.sorted(by: (isDescending ? backward : forward))
