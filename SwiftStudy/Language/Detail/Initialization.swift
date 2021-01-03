4582805018bc0c3bae07442d83042d9cece5b739
huangjianwu <kenmuhuang@xinli001.com>
2021-01-03 22:53:51 +0800

    [test] 构造过程 - 类的继承和构造过程；https://swiftgg.gitbook.io/swift/swift-jiao-cheng/14_initialization#class-inheritance-and-initialization

--- SwiftStudy/Language/Detail/Initialization.swift
+++ SwiftStudy/Language/Detail/Initialization.swift
@@ -157,12 +157,6 @@ class Initialization: MainProtocol {
         print(zeroByZero.width, zeroByZero.height)
     }
 
-    class ShoppingListItem {
-        var name: String?
-        var quantity = 1
-        var purchased = false
-    }
-
     struct Size {
         var width = 0.0, height = 0.0
     }
@@ -210,25 +204,189 @@ class Initialization: MainProtocol {
     // MARK: - 类的继承和构造过程
     private func classInheritanceAndInitialization() {
         // 指定构造器和便利构造器
+        /*
+         类里面的所有存储型属性——包括所有继承自父类的属性——都必须在构造过程中设置初始值。
+
+         Swift 为类类型提供了两种构造器来确保实例中所有存储型属性都能获得初始值，它们被称为指定构造器和便利构造器。
+         */
 
 
         // 指定构造器和便利构造器的语法
+        /*
+         init(parameters) {
+            statements
+         }
+
+         convenience init(parameters) {
+            statements
+         }
+         */
 
 
         // 类类型的构造器代理
+        /*
+         为了简化指定构造器和便利构造器之间的调用关系，Swift 构造器之间的代理调用遵循以下三条规则：
+
+         规则 1
+         指定构造器必须调用其直接父类的的指定构造器。
+
+         规则 2
+         便利构造器必须调用同类中定义的其它构造器。
+
+         规则 3
+         便利构造器最后必须调用指定构造器。
+
+         一个更方便记忆的方法是：
+         1、指定构造器必须总是向上代理
+         2、便利构造器必须总是横向代理
+         */
 
 
         // 两段式构造过程
+        /*
+         Swift 中类的构造过程包含两个阶段。第一个阶段，类中的每个存储型属性赋一个初始值。当每个存储型属性的初始值被赋值后，第二阶段开始，它给每个类一次机会，在新实例准备使用之前进一步自定义它们的存储型属性。
 
+         两段式构造过程的使用让构造过程更安全，同时在整个类层级结构中给予了每个类完全的灵活性。两段式构造过程可以防止属性值在初始化之前被访问，也可以防止属性被另外一个构造器意外地赋予不同的值。
+
+         注意：Swift 的两段式构造过程跟 Objective-C 中的构造过程类似。最主要的区别在于阶段 1，Objective-C 给每一个属性赋值 0 或空值（比如说 0 或 nil）。Swift 的构造流程则更加灵活，它允许你设置定制的初始值，并自如应对某些属性不能以 0 或 nil 作为合法默认值的情况。
+
+         阶段 1
+         类的某个指定构造器或便利构造器被调用。
+         完成类的新实例内存的分配，但此时内存还没有被初始化。
+         指定构造器确保其所在类引入的所有存储型属性都已赋初值。存储型属性所属的内存完成初始化。
+         指定构造器切换到父类的构造器，对其存储属性完成相同的任务。
+         这个过程沿着类的继承链一直往上执行，直到到达继承链的最顶部。
+         当到达了继承链最顶部，而且继承链的最后一个类已确保所有的存储型属性都已经赋值，这个实例的内存被认为已经完全初始化。此时阶段 1 完成。
+
+         阶段 2
+         从继承链顶部往下，继承链中每个类的指定构造器都有机会进一步自定义实例。构造器此时可以访问 self、修改它的属性并调用实例方法等等。
+         最终，继承链中任意的便利构造器有机会自定义实例和使用 self。
+         */
 
         // 构造器的继承和重写
+        /*
+         跟 Objective-C 中的子类不同，Swift 中的子类默认情况下不会继承父类的构造器。Swift 的这种机制可以防止一个父类的简单构造器被一个更精细的子类继承，而在用来创建子类时的新实例时没有完全或错误被初始化。
+
+         注意：当你重写一个父类的指定构造器时，你总是需要写 override 修饰符，即使是为了实现子类的便利构造器。
+         相反，如果你编写了一个和父类便利构造器相匹配的子类构造器，由于子类不能直接调用父类的便利构造器，因此，严格意义上来讲，你的子类并未对一个父类构造器提供重写。最后的结果就是，你在子类中“重写”一个父类便利构造器时，不需要加 override 修饰符。
+
+         注意：子类可以在构造过程修改继承来的变量属性，但是不能修改继承来的常量属性。
+         */
+        let vehicle = Vehicle()
+        print("Vehicle: \(vehicle.description)")
+
+        let bicycle = Bicycle()
+        print("Bicycle: \(bicycle.description)")
+
+        let hoverboard = Hoverboard(color: "silver")
+        print("Hoverboard: \(hoverboard.description)")
 
 
         // 构造器的自动继承
+        /*
+         如上所述，子类在默认情况下不会继承父类的构造器。但是如果满足特定条件，父类构造器是可以被自动继承的。事实上，这意味着对于许多常见场景你不必重写父类的构造器，并且可以在安全的情况下以最小的代价继承父类的构造器。
+         假设你为子类中引入的所有新属性都提供了默认值，以下 2 个规则将适用：
+
+         规则 1
+         如果子类没有定义任何指定构造器，它将自动继承父类所有的指定构造器。
+
+         规则 2
+         如果子类提供了所有父类指定构造器的实现——无论是通过规则 1 继承过来的，还是提供了自定义实现——它将自动继承父类所有的便利构造器。
+
+         即使你在子类中添加了更多的便利构造器，这两条规则仍然适用。
+
+         注意：子类可以将父类的指定构造器实现为便利构造器来满足规则 2。
+         */
 
 
         // 指定构造器和便利构造器实践
+        var namedMeat = Food()
+        print(namedMeat.name)
 
+        namedMeat = Food(name: "Bacon")
+        print(namedMeat.name)
+
+        let oneMysteryItem = RecipeIngredient()
+        print(oneMysteryItem.name, oneMysteryItem.quantity)
+
+        let oneBacon = RecipeIngredient(name: "Bacon")
+        print(oneBacon.name, oneBacon.quantity)
+
+        let sixEggs = RecipeIngredient(name: "Eggs", quantity: 6)
+        print(sixEggs.name, sixEggs.quantity)
+
+        var breakfastList = [
+            ShoppingListItem(),
+            ShoppingListItem(name: "Bacon"),
+            ShoppingListItem(name: "Eggs", quantity: 6),
+        ]
+        breakfastList[0].name = "Orange juice"
+        breakfastList[0].purchased = true
+        for item in breakfastList {
+            print(item.description)
+        }
+    }
+
+    class Vehicle {
+        var numberOfWheels = 0
+        var description: String {
+            return "\(numberOfWheels) wheel(s)"
+        }
+    }
+
+    class Bicycle: Vehicle {
+        override init() {
+            super.init()
+
+            numberOfWheels = 2
+        }
+    }
+
+    class Hoverboard: Vehicle {
+        var color: String
+        init(color: String) {
+            self.color = color
+
+            // super.init() 在这里被隐式调用
+        }
+        override var description: String {
+            return "\(super.description) in a beautiful \(color)"
+        }
+    }
+
+    class Food {
+        var name: String
+
+        init(name: String) {
+            self.name = name
+        }
+
+        convenience init() {
+            self.init(name: "[Unnamed]")
+        }
+    }
+
+    class RecipeIngredient: Food {
+        var quantity: Int
+
+        init(name: String, quantity: Int) {
+            self.quantity = quantity
+
+            super.init(name: name)
+        }
+
+        override convenience init(name: String) {
+            self.init(name: name, quantity: 1)
+        }
+    }
+
+    class ShoppingListItem: RecipeIngredient {
+        var purchased = false
+        var description: String {
+            var output = "\(quantity) x \(name) "
+            output += purchased ? "✔" : "✘"
+            return output
+        }
     }
 
     // MARK: - 可失败构造器
